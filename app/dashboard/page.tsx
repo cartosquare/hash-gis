8d6f404519abff6a289a9e8014da3e7f635a1d63
XuXiang <cartosquare@163.com>
2023-06-29 23:04:11 +0800

    open/save files; vis in map container

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,75 @@
 "use client"
 
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
+import Map from '../map';
 import { useRouter } from 'next/navigation';
+import { open, save } from "@tauri-apps/api/dialog"
+import { useEffect, useState } from 'react';
+import L, { latLngBounds } from 'leaflet';
 
-const MapWithNoSSR = dynamic(() => import('../map'), {
-  ssr: false,
-});
+// const MapWithNoSSR = dynamic(() => import('../map'), {
+//   ssr: false,
+// });
 
 export default function Home() {
   const router = useRouter();
+  const [inputFile, setInputFile] = useState<string>("");
+  const [outputFile, setOutputFile] = useState<string>("");
+  const [layers, setLayers] = useState<string[]>([]);
+  const [bounds, setBounds] = useState<L.LatLngBounds>();
 
-  const navigator_home = () => {
+  const navigatorHome = () => {
     router.push("/");
+  }
+
+  const createMapLayer = async (filepath: string) => {
+    try {
+      const rawResponse = await fetch(`http://localhost:8080/map`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'name': "",
+          "path": filepath,
+          "style": {
+            "colours": [
+              [0, 0, 0], [255, 255, 255]
+            ],
+            "bands": [1, 2, 3]
+          }
+        })
+      });
+      if (rawResponse.status != 200) {
+        //toast.error(`请求失败：${rawResponse.status}`);
+        console.log('error');
+      } else {
+        const response = (await rawResponse.json());
+        console.log(response)
+        setLayers([`http://localhost:8080/${response.name}/{z}/{x}/{y}.png`]);
+        const b = new L.LatLngBounds(L.latLng(response.bounds[0], response.bounds[1]), L.latLng(response.bounds[2], response.bounds[3]))
+        console.log(b)
+        setBounds(b
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      // toast.error(`请求失败：${error}`);
+      console.log('error!', error);
+    }
+  }
+
+  const openInputFile = async () => {
+    const file = await open();
+    setInputFile(file as string);
+
+    // create map view
+    await createMapLayer(file as string);
+  }
+
+  const saveOutputFile = async () => {
+    const file = await save();
+    setOutputFile(file as string);
   }
 
   return (
@@ -19,9 +77,9 @@ export default function Home() {
       <div className='flex flex-row w-full grow bg-base-300 p-4'>
 
         <div className='flex flex-col gap-4'>
-          <button className='btn w-32' onClick={navigator_home}>
+          <button className='btn w-32' onClick={navigatorHome}>
 
-            <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="19" height="19"><path d="M8 1L1 7.5 8 14m5.5-13l-7 6.5 7 6.5" stroke="currentColor" stroke-linecap="square"></path></svg>
+            <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="19" height="19"><path d="M8 1L1 7.5 8 14m5.5-13l-7 6.5 7 6.5" stroke="currentColor" strokeLinecap="square"></path></svg>
             返回
           </button>
           <div tabIndex={0} className="collapse collapse-open bg-base-200">
@@ -35,14 +93,21 @@ export default function Home() {
               <div className='form-control'>
                 <label className='label cursor-pointer'>
                   <span className="label-text w-24">输入</span>
-                  <input type="file" className="file-input file-input-sm file-input-bordered w-full max-w-xs" />
+                  <div className='join'>
+                    <input type="text" value={inputFile} className="input join-item" readOnly />
+                    <button onClick={openInputFile} className='btn btn-neutral join-item'>选择..</button>
+                  </div>
                 </label>
               </div>
 
               <div className='form-control'>
                 <label className='label cursor-pointer'>
                   <span className="label-text w-24">输出</span>
-                  <input type="file" className="file-input file-input-sm file-input-bordered w-full max-w-xs" />
+                  <div className='join'>
+                    <input type="text" value={outputFile} className="input join-item" readOnly />
+                    <button onClick={saveOutputFile} className='btn btn-neutral join-item'>选择..</button>
+                  </div>
+
                 </label>
               </div>
 
@@ -95,7 +160,11 @@ export default function Home() {
         </div>
 
 
-        <MapWithNoSSR />
+        {/* <MapWithNoSSR /> */}
+        <Map
+          layers={layers}
+          bounds={bounds}
+        />
       </div>
     </main>
   )
--- app/map.tsx
+++ app/map.tsx
@@ -1,9 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, useMap, FeatureGroup, Popup } from 'react-leaflet';
-import { useRef, useState } from 'react';
 import L, { LatLngExpression } from 'leaflet';
-// import 'leaflet-draw/dist/leaflet.draw.css';
-// import { EditControl } from 'react-leaflet-draw';
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: ('leaflet/images/marker-icon-2x.png'),
@@ -17,22 +14,33 @@ export function ChangeView({ coords }: { coords: LatLngExpression }) {
   return null;
 }
 
-export default function Map(){
- const [geoData, setGeoData] = useState({ lat: 27.67993181471809, lng: 118.3639870939991 });
-  const center: LatLngExpression = [geoData.lat, geoData.lng];
-  return (
-    <MapContainer className='flex grow' center={center} zoom={12}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.maxar.com/">Maxar</a>'
-        url='https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.webp?sku=101nCfuWrTLLf&access_token=pk.eyJ1IjoiYXZvbG92aWsiLCJhIjoiY2txdzNpdWs1MGkwZjJ3cGNrYnZua3I4aCJ9.Le6NapjFYy5FfdDXfBmvrg'
-      />
-      <ChangeView coords={center} />
-      <Marker position={center}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+export function BoundsView({ bounds }: { bounds: L.LatLngBounds | undefined }) {
+  const map = useMap();
+  if (bounds) {
+    map.fitBounds(bounds)
+  }
+  return null;
+}
 
+
+export default function Map({
+  layers,
+  bounds,
+}: {
+  layers: string[],
+  bounds: L.LatLngBounds | undefined,
+}) {
+  return (
+    <MapContainer className='flex grow' bounds={bounds}>
+      {
+        layers && layers.map((url, index) => (
+          <TileLayer
+            attribution='&copy; <a href="https://www.rs.sensetime.com/">SenseTime</a>'
+            url={url}
+          />
+        ))
+      }
+      <BoundsView bounds={bounds} />
     </MapContainer>
   );
 }
